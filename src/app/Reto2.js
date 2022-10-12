b3a9fab84b1780ff250885af0976388999c70fa2
Antonio Hernandez <antoniohm.ing@gmail.com>
2022-10-12 10:54:07 -0500

    challengetwojs

---
+++ src/app/Reto2.js
@@ -0,0 +1,6 @@
+const personajesHOD =  ["Rhanereya", "Daemon", "Viserys,", "Alicent"];
+
+for (let i = 0; i < personajesHOD.length; i++) {
+    
+    console.log(`${i + 1}. ${personajesHOD[i]}`);
+}
